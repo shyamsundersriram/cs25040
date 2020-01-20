484b78605ea8b242c9b167d4d8ce18bd6b02396b
shyamsundersriram <shyamsundersriram@outlook.com>
2020-01-19 20:18:27 -0600

    convolution done w/ tests

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -152,64 +152,6 @@ def conv_2d(image, filt, mode='zero'):
       result[row - pad, col - pad] = np.sum(np.multiply(crop, filt))    
   return result 
 
-def test_conv2d(mode='zero'): 
-  import scipy.signal
-
-  image1 = np.array([[5, 10, 3, 2, 9], 
-                    [6, 4, 2, 3, 8], 
-                    [1, 4, 7, 1, 2 ],
-                    [6, 5, 2, 3, 9],
-                    [7, 3, 5, 1, 0], 
-                    [4, 5, 3, 2, 2]])
-
-  filt1 = np.array([[1, 0, 3], [0, 3, 1], [-1, 2, 0]])
-
-  filt2 = np.array([[1, 0, 3]])
-
-  big_image = np.array([[25, 100, 75, 49, 130], 
-                    [50, 80, 0, 70, 100], 
-                    [5, 10, 10, 30, 0 ],
-                    [60, 50, 12, 29, 32],
-                    [37, 33, 55, 21, 90], 
-                    [140, 17, 0, 23, 222]])
-
-  filt3 = np.array([[1, 0, 1], [0, 1, 0], [0, 0, 1]])
-
-  big_filt = np.array([[1, 0, 1, 1, 1], 
-                   [0, 1, 0, 1, 1],
-                   [0, 0, 1, 1, 1], 
-                   [1, 1, 1, 1, 1], 
-                   [1, 1, 1, 1, 1]])
-
-  mine = conv_2d(big_image, filt2, mode)
-  actual = scipy.signal.convolve2d(big_image, filt2, mode='same', boundary='fill', fillvalue=0)
-  return np.array_equal(mine, actual)
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # for l in range((filt_l // 2, length + filt_l // 2)): 
-  #   for w in range((filt_w // 2, width + filt_w // 2)):
-  #     crop = image[l - filt_l // 2: l + filt_l // 2, w - filt_w // 2, w + filt_w // 2]
-  #     crop = image[l:(l + 2*filt_l), w:(w + 2*filt_w)]
-  #     result[l - filt_l // 2, w - filt_w //2 ] = sum(np.multiply(np.array(crop), np.array(filt))) 
-
-
-
-
-
-
-
-
 """
    GAUSSIAN DENOISING (5 Points)
 
